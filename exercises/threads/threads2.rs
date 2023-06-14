3b4c4f90817873084ee1f239057ec22d24c12465
Zourunfa <473023989@qq.com>
2023-06-14 17:40:21 +0800

    feat:thread

--- exercises/threads/threads2.rs
+++ exercises/threads/threads2.rs
@@ -1,11 +1,4 @@
-// threads2.rs
-// Execute `rustlings hint threads2` or use the `hint` watch subcommand for a hint.
-// Building on the last exercise, we want all of the threads to complete their work but this time
-// the spawned threads need to be in charge of updating a shared value: JobStatus.jobs_completed
-
-// I AM NOT DONE
-
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -14,14 +7,19 @@ struct JobStatus {
 }
 
 fn main() {
-    let status = Arc::new(JobStatus { jobs_completed: 0 });
+    /*
+    封装了一个Mutex类型的互斥锁和一个JobStatus类型的共享数据。Arc类型的智能指针可以在多个线程之间共享，而Mutex类型的互斥锁可以确保
+    在同一时间只有一个线程可以访问共享数据，从而避免竞态条件的发生。
+     */
+    let status = Arc::new(Mutex::new(JobStatus { jobs_completed: 0 }));
     let mut handles = vec![];
     for _ in 0..10 {
         let status_shared = Arc::clone(&status);
         let handle = thread::spawn(move || {
             thread::sleep(Duration::from_millis(250));
             // TODO: You must take an action before you update a shared value
-            status_shared.jobs_completed += 1;
+            let mut status = status_shared.lock().unwrap();
+            status.jobs_completed += 1;
         });
         handles.push(handle);
     }
@@ -29,6 +27,7 @@ fn main() {
         handle.join().unwrap();
         // TODO: Print the value of the JobStatus.jobs_completed. Did you notice anything
         // interesting in the output? Do you have to 'join' on all the handles?
-        println!("jobs completed {}", ???);
+        let status = status.lock().unwrap();
+        println!("jobs completed {}", status.jobs_completed);
     }
 }
