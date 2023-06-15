de3c75b93ab707556860397b20a1a5a8fd51464e
Zourunfa <473023989@qq.com>
2023-06-15 16:49:11 +0800

    feat: smart_points

--- exercises/smart_pointers/box1.rs
+++ exercises/smart_pointers/box1.rs
@@ -16,11 +16,23 @@
 //
 // Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+/*
+在编译时，Rust 需要知道一个类型占用多少空间。
+这对于递归类型来说会有问题，因为一个值可能包含另一个相同类型的值。
+为了解决这个问题，我们可以使用 Box - 一种智能指针，用于在堆上存储数据，
+同时也允许我们包装递归类型。
+
+在这个练习中，我们要实现的递归类型是 cons list -
+一种在函数式编程语言中经常出现的数据结构。每个元素包含两个部分：
+当前元素的值和下一个元素。最后一个元素是一个称为 Nil 的值。
+
+在 Rust 中，递归类型需要使用 Box 或者其他智能指针来实现。这是因为 Rust 需要在编译时知道类型的大小，而递归类型的大小是无限的，无法得知。
+使用 Box 可以将递归类型的所有元素都存储在堆上，从而避免了大小无限的问题。
+ */
 
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -33,11 +45,11 @@ fn main() {
 }
 
 pub fn create_empty_list() -> List {
-    todo!()
+    List::Nil
 }
 
 pub fn create_non_empty_list() -> List {
-    todo!()
+    List::Cons(2, Box::new(List::Nil))
 }
 
 #[cfg(test)]
--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -1,8 +1,6 @@
 // threads3.rs
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::sync::mpsc;
 use std::sync::Arc;
 use std::thread;
@@ -29,10 +27,17 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     let qc1 = Arc::clone(&qc);
     let qc2 = Arc::clone(&qc);
 
+    /*
+    这里需要使用 tx.clone() 方法的原因是，mpsc::Sender 类型是不可复制（non-copyable）的。这意味着，如果我们直接将 tx 变量传递给 send_tx 函数，那么在函数中使用 tx 变量时，
+    就会将其所有权转移给函数，导致在函数返回后无法再次使用 tx 变量。
+     */
+    let tx1 = tx.clone();
+    let tx2 = tx.clone();
+
     thread::spawn(move || {
         for val in &qc1.first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx1.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
@@ -40,13 +45,15 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     thread::spawn(move || {
         for val in &qc2.second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx2.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
 }
 
 fn main() {
+    // mpsc::channel() 函数创建了一个通道，并返回两个端点：一个发送者（Sender）和一个接收者（Receiver）
+    //将发送者和接收者分别赋值给了 tx 和 rx 变量。
     let (tx, rx) = mpsc::channel();
     let queue = Queue::new();
     let queue_length = queue.length;
